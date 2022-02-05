80f1accc89cabaeca13c87eab77e28cc152e9055
nubeliefs <non.universal.beliefs@gmail.com>
2022-02-06 02:24:32 +0530

    ee

--- nubeliefs_files/app.js
+++ nubeliefs_files/app.js
@@ -21,3 +21,20 @@ app.config(function($routeProvider) {
     })
     ;
 });
+
+//Microsoft Clarity - Google Analytics
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "adpinvtq7u");
+
+//Global site tag (gtag.js) - Google Analytics
+window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-F96R1MZMEQ');
+        
+
+
