1b8371370f00194ac2cb4dc3e762261a2974bbc7
nubeliefs <non.universal.beliefs@gmail.com>
2022-02-06 02:36:24 +0530

    abcd

--- nubeliefs_files/app.js
+++ nubeliefs_files/app.js
@@ -35,6 +35,34 @@ window.dataLayer = window.dataLayer || [];
         gtag('js', new Date());
 
         gtag('config', 'G-F96R1MZMEQ');
+
+//scroll top JS
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
         
 
 
