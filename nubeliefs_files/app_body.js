5e664d9d8f4ab74a9bf42eef679678d723260a5e
nubeliefs <non.universal.beliefs@gmail.com>
2022-02-06 02:56:34 +0530

    hhh

--- nubeliefs_files/app_body.js
+++ nubeliefs_files/app_body.js
@@ -29,10 +29,20 @@ function backToTop() {
 //loader
 var preloader = document.getElementById("loading");
 
-        function myFunction() {
-            preloader.style.display = 'none';
-            preloader.setAttribute("class", "invisible");
-        };
+function myFunction() {
+    preloader.style.display = 'none';
+    preloader.setAttribute("class", "invisible");
+};
+
+//statcounter
+var sc_project = 12706895;
+var sc_invisible = 0;
+var sc_security = "dfb6332b";
+var sc_text = 2;
+var scJsHost = "https://";
+document.write("<sc" + "ript type='text/javascript' src='" +
+    scJsHost +
+    "statcounter.com/counter/counter.js'></" + "script>");
 
 
         
